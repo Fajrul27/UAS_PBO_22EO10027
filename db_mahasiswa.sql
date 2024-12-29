44a2b36a7e3f6c39a0b9ebd643ed709bda9e3a7f
Fajrul27 <160477968+Fajrul27@users.noreply.github.com>
2024-12-29 20:20:11 +0700

    Add files via upload

---
+++ db_mahasiswa.sql
@@ -0,0 +1,73 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Dec 29, 2024 at 02:19 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_mahasiswa`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `anggota`
+--
+
+CREATE TABLE `anggota` (
+  `id_anggota` int(11) NOT NULL,
+  `nama` varchar(100) NOT NULL,
+  `nim` varchar(100) DEFAULT NULL,
+  `no_telepon` varchar(15) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `fakultas` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `anggota`
+--
+
+INSERT INTO `anggota` (`id_anggota`, `nama`, `nim`, `no_telepon`, `foto`, `fakultas`) VALUES
+(12, 'dimas', '22EO10017', '08876546666', NULL, '1'),
+(27, 'Ahmad Sholehudin', '22EO10028', '086573687289', NULL, '5'),
+(28, 'ayamgeprk', '22EO987666', '0864562727828', NULL, '3'),
+(29, 'Jane Doe', 'jane@example.com', '123456789', NULL, '2'),
+(30, 'Ahmad Fajrul \'Ulum', '22EO10027', '082323446380', NULL, '5');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `anggota`
+--
+ALTER TABLE `anggota`
+  ADD PRIMARY KEY (`id_anggota`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `anggota`
+--
+ALTER TABLE `anggota`
+  MODIFY `id_anggota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
